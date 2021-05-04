e6c0c832c247d5474d723656b09773881b27dbbd
tgdraugr <tiagogabrielsilva0@gmail.com>
2021-05-04 17:21:08 +0100

    Removal of non needed directories

--- StateMachines.Messages/ISayGoodbye.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StateMachines.Messages 
-{
-    public interface ISayGoodbye 
-    {
-        public string Sender { get; }
-    }
-}
--- StateMachines.Messages/ISayHello.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StateMachines.Messages 
-{
-    public interface ISayHello 
-    {
-        public string Sender { get; }
-    }
-}
